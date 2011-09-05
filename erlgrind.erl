d8e965fd9348f2510b43423b9600502989335efe
Isac Sacchi e Souza <isacssouza@gmail.com>
2011-09-04 22:28:11 -0300

    Improve conversion.

--- erlgrind.erl
+++ erlgrind.erl
@@ -5,8 +5,8 @@
 
 convert(InFileName, OutFileName) ->
     {ok, OutFile} = file:open(OutFileName, [write]),
-    {ok, Terms} = file:consult(FileName),
-    io:format(OutFile, "events: Instructions~n"),
+    {ok, Terms} = file:consult(InFileName),
+    io:format(OutFile, "events: Instructions~n", []),
     process_terms(OutFile, Terms).
 
 process_terms(OutFile, []) ->
@@ -19,8 +19,9 @@ process_terms(OutFile, [Entry | Rest]) ->
     process_entry(OutFile, Entry),
     process_terms(OutFile, Rest).
 
-process_entry(OutFile, {Calling, Actual, Called}) ->
-    process_actual(OutFile, Actual).
+process_entry(OutFile, {CallingList, Actual, CalledList}) ->
+    process_actual(OutFile, Actual),
+    process_called_list(OutFile, CalledList).
 
 process_actual(_, {suspend, Cnt, Acc, Own}) ->
     ok;
@@ -30,5 +31,23 @@ process_actual(_, {undefined, Cnt, Acc, Own}) ->
     ok;
 process_actual(OutFile, {{Mod, Func, Arity}, Cnt, Acc, Own}) ->
     io:format(OutFile, "fl=~w.erl~n", [Mod]),
-    io:format(OutFile, "fn=~w_~w~n", [Func, Arity]),
-    io:format(OutFile, "1 ~w~n", [Own]).
+    io:format(OutFile, "fn=~w/~w~n", [Func, Arity]),
+    io:format(OutFile, "1 ~w~n", [trunc(Own*1000)]).
+
+process_called_list(_, []) ->
+    ok;
+process_called_list(OutFile, [Called | Rest]) ->
+    process_called(OutFile, Called),
+    process_called_list(OutFile, Rest).
+
+process_called(_, {suspend, Cnt, Acc, Own}) ->
+    ok;
+process_called(_, {garbage_collect, Cnt, Acc, Own}) ->
+    ok;
+process_called(_, {undefined, Cnt, Acc, Own}) ->
+    ok;
+process_called(OutFile, {{Mod, Func, Arity}, Cnt, Acc, Own}) ->
+    io:format(OutFile, "cfl=~w.erl~n", [Mod]),
+    io:format(OutFile, "cfn=~w/~w~n", [Func, Arity]),
+    io:format(OutFile, "calls=~w 1~n", [Cnt]),
+    io:format(OutFile, "1 ~w~n", [trunc(Own*1000)]).
